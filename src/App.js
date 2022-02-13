ecc1e3de5b339f3850282a81ef2cb35e17574f93
ameysunu <ameysunu00719@gmail.com>
2022-02-13 16:38:08 +0000

    code formatting

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import Home from "./pages/home";
 import Secret from "./pages/protected/secrets";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import AppBar from "./Navbar";
-import {initializeApp} from 'firebase/app';
-import {firebaseConfig} from './config/firebaseConfig';
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./config/firebaseConfig";
 
 function App() {
   initializeApp(firebaseConfig);
   return (
     <BrowserRouter>
-    <AppBar />
-    <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/register" component={Register}/>
-          <Route exact path="/login" component={Login}/>
-          <Route exact path="/protected" component={Secret}/>
-    </Switch>
+      <AppBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/protected" component={Secret} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,53 +1,56 @@
-import {Card, Button, Alert} from "react-bootstrap";
+import { Card, Button, Alert } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
-function Home(){
-    const [greeting, setGreeting] = useState("");
-    const [show, setShow] = useState(false);
+function Home() {
+  const [greeting, setGreeting] = useState("");
+  const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const openPop = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const openPop = () => setShow(true);
 
-    useEffect(() => {
-        var d = new Date();
-        var hours = d.getHours();
+  useEffect(() => {
+    var d = new Date();
+    var hours = d.getHours();
 
-    
-        if (hours < 12) {
-          setGreeting("Good Morning!");
-        } else if (hours < 18) {
-          setGreeting("Good Afternoon!");
-        } else {
-          setGreeting("Good Evening!");
-        }
-      }, []);
-    
-    return (
-        <div style={{padding: "10px", color: "white"}} className="body">
-           <Card border="dark" bg= "dark">
-           <Card.Title style={{padding: "10px"}}>
-           <h2>{greeting}</h2>
-           </Card.Title>
-  <Card.Body>
-  <Button variant="outline-light" onClick={openPop}>More Info</Button>{' '}
-  </Card.Body>
-</Card>
-<div style={{paddingTop: "10px"}}>
-<Alert show={show} variant="success" onClose={handleClose}>
+    if (hours < 12) {
+      setGreeting("Good Morning!");
+    } else if (hours < 18) {
+      setGreeting("Good Afternoon!");
+    } else {
+      setGreeting("Good Evening!");
+    }
+  }, []);
+
+  return (
+    <div style={{ padding: "10px", color: "white" }} className="body">
+      <Card border="dark" bg="dark">
+        <Card.Title style={{ padding: "10px" }}>
+          <h2>{greeting}</h2>
+        </Card.Title>
+        <Card.Body>
+          <Button variant="outline-light" onClick={openPop}>
+            More Info
+          </Button>{" "}
+        </Card.Body>
+      </Card>
+      <div style={{ paddingTop: "10px" }}>
+        <Alert show={show} variant="success" onClose={handleClose}>
           <Alert.Heading>Hospitals</Alert.Heading>
           <p>
-            This application creates a MANET which can connect various hospitals, i.e. nodes, to provide patient details to the hospital, with equipments that only certain hospitals have.
+            This application creates a MANET which can connect various
+            hospitals, i.e. nodes, to provide patient details to the hospital,
+            with equipments that only certain hospitals have.
           </p>
           <hr />
           <div className="d-flex justify-content-end">
-          <Button onClick={() => setShow(false)} variant="outline-success">
-            Done
-          </Button>
-        </div>
+            <Button onClick={() => setShow(false)} variant="outline-success">
+              Done
+            </Button>
+          </div>
         </Alert>
-</div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
