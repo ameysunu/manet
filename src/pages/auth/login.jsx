e473f95bd5ffb47ed6e9aec9f9b4d6bc59ce4947
ameysunu <ameysunu00719@gmail.com>
2022-02-13 16:51:40 +0000

    auth error handled

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner } from "react-bootstrap";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const auth = getAuth();
   const handleLogin = () => {
+    setLoading(true);
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         console.log("Signed in user: ", user);
+        setLoading(false);
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log("An error occured: ", errorCode, errorMessage);
+        setLoading(false);
+        setError(errorCode + errorMessage);
       });
   };
   return (
@@ -45,6 +51,13 @@ const Login = () => {
         <Button variant="primary" onClick={handleLogin}>
           Login
         </Button>
+       {loading && <div style={{ position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",}}>
+        <Spinner animation="border" variant="primary" />
+        </div> } 
+        {error && <div style={{ color: "red", paddingTop: "10px" }}>{error}</div>}
       </Form>
     </div>
   );
--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner } from "react-bootstrap";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const auth = getAuth();
   const handleRegister = () => {
+    setLoading(true);
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         const user = userCredential.user;
         console.log("Registered user: ", user);
         setEmail("");
         setPassword("");
+        setLoading(false);
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log("Error ocured: ", errorCode, errorMessage);
+        setLoading(false);
+        setError(errorCode + errorMessage);
       });
   };
   return (
@@ -47,6 +53,13 @@ const Register = () => {
         <Button variant="primary" onClick={handleRegister}>
           Register
         </Button>
+        {loading && <div style={{ position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",}}>
+        <Spinner animation="border" variant="primary" />
+        </div> } 
+        {error && <div style={{ color: "red", paddingTop: "10px" }}>{error}</div>}
       </Form>
     </div>
   );
