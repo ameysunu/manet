d6d912565d1d528bd86bfa8cb192350b4397b48f
ameysunu <ameysunu00719@gmail.com>
2022-02-13 22:08:53 +0000

    firebase data mapped

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,18 +1,36 @@
-import { useLocation } from "react-router-dom";
-import {useEffect} from "react";
+import { useEffect, useState } from "react";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../config/firebaseConfig";
+import { Card } from "react-bootstrap";
 
 function Main() {
-    const location = useLocation();
-    useEffect(() => {  
-        console.log("userID: ", location.state);
-    }, [location]);
+  const [hospitals, setHospitals] = useState([]);
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  useEffect(() => {
+    const getData = async () => {
+      const hospitalData = await getDocs(collection(db, "hospitals"));
+      setHospitals(
+        hospitalData.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+      console.log(hospitalData.docs);
+    };
+    getData();
+  }, []);
 
-    return (
-        <div style={{padding:"10px"}}>
-            <h1>List of Hospitals</h1>
-            <p>UID: {location.state}</p>
+  return (
+    <div style={{ padding: "10px" }}>
+      <h1>List of Hospitals</h1>
+      {hospitals.map((data) => (
+        <div key={data.id} style={{ padding: "10px" }}>
+          <Card>
+            <Card.Body>{data.id}</Card.Body>
+          </Card>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Main;
