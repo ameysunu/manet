c7c496f51fd38bd9e8a9081c83b19dd583b3885f
ameysunu <ameysunu00719@gmail.com>
2022-02-15 14:21:29 +0000

    secrets deleted

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import Home from "./pages/home";
-import Secret from "./pages/protected/secrets";
 import Main from "./pages/main";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AppBar from "./Navbar";
@@ -18,7 +17,6 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/protected" component={Secret} />
         <Route exact path="/main" component={Main}/>
       </Switch>
     </BrowserRouter>
