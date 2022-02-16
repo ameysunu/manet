0ac81825ea70324ee0083420f85d4bab9554d0c3
ameysunu <ameysunu00719@gmail.com>
2022-02-16 16:58:12 +0000

    pop added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/main" component={Main}/>
         {/* test */}
+        {/* pop() */}
       </Switch>
     </BrowserRouter>
   );
